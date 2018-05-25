3733b08c277ea607ea47c6389ba10759e18abdb9
Jianshen Liu <jliu120@ucsc.edu>
2018-05-25 15:34:07 -0600

    Add manifest push to the end of each loop

--- push.sh
+++ push.sh
@@ -14,4 +14,5 @@ for tag in "${TAGS[@]}"; do
     docker manifest create "${IMAGE_NAME}":"${tag}" "${IMAGE_NAME}":amd64-"${tag}" "${IMAGE_NAME}":arm64v8-"${tag}"
     docker manifest annotate "${IMAGE_NAME}":"${tag}" "${IMAGE_NAME}":amd64-"${tag}" --os linux --arch amd64
     docker manifest annotate "${IMAGE_NAME}":"${tag}" "${IMAGE_NAME}":arm64v8-"${tag}" --os linux --arch arm64 --variant v8
+    docker manifest push --purge "${IMAGE_NAME}":"${tag}"
 done
